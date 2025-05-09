47d4c6ab1c02975b87c3e9ca36b1745506728ce6
Dylan Figueredo <d.figueredo2104@gmail.com>
2025-05-08 19:47:55 -0500

    changed db.py to this

---
+++ connection.py
@@ -0,0 +1,27 @@
+import psycopg2
+import logging
+import os
+from psycopg2 import DatabaseError 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_NAME = os.environ.get("DB_NAME")
+USER_NAME = os.environ.get("USER_NAME")
+PASSWORD = os.environ.get("PASSWORD")
+HOST = os.environ.get("HOST")
+PORT = os.environ.get("PORT", "5432")
+
+def get_connection():
+    try:
+        logging.info("Connecting to the database...")
+        return psycopg2.connect(
+            dbname=DB_NAME,
+            user=USER_NAME,
+            password=PASSWORD,
+            host=HOST,
+            port=PORT
+        )
+    except DatabaseError as e:
+        logging.error(f"Database connection error: {e}")
+        raise e
